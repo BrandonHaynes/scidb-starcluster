3035f09391fea4240f614dfea0824635d327c3bd
Brandon Haynes <bhaynes@cs.washington.edu>
2015-01-05 23:23:48 -0800

    Better path handling on INCLUDE modification

--- setup.py
+++ setup.py
@@ -1,23 +1,25 @@
 import os
 import getpass
+import re
 from setuptools import setup
 from setuptools.command.install import install
 
 plugin_name = 'scidbplugin.py'
 config_name = 'scidbcluster.config'
-
+config_path = '~/.starcluster'
 
 class RegisterPluginDecorator(install):
     def run(self):
         install.run(self)
 
-        config_filename = '~/.starcluster/config'
+        config_filename = os.path.join(config_path, 'config')
         config = self.get_config(config_filename)
-        if not 'INCLUDE=~/.starcluster/' + config_name in config:
+
+        if not self.already_included(config_name, config):
             print 'Adding include to ' + config_filename
             self.add_include(config, config_filename)
 
-        self.set_keyname('~/.starcluster/' + config_name)
+        self.set_keyname(os.path.join(config_path, config_name))
 
     @staticmethod
     def get_config(path):
@@ -25,19 +27,33 @@ class RegisterPluginDecorator(install):
             return file.read()
 
     @staticmethod
+    def already_included(config_name, config):
+        return re.search(r'^INCLUDE\w*=\w*.*' + re.escape(os.path.join(config_path, config_name)),
+                         config, 
+                         flags=re.IGNORECASE | re.MULTILINE)
+
+    @staticmethod
     def add_include(config, path):
-        insertion = '[global]\nINCLUDE=~/.starcluster/{}\n'.format(config_name)
-        if '[global]' in config:
-            config = config.replace('[global]', insertion)
+        insertion = 'INCLUDE={}'.format(os.path.join(config_path, config_name))
+
+        # No global section
+        if not '[global]' in config:
+            config = '[global]\n{}\n{}'.format(insertion, config)
+        # Global section with existing INCLUDE pair
+        elif re.search(r'^INCLUDE\w*=\w*', config, flags=re.IGNORECASE | re.MULTILINE):
+            config = re.sub(r'^INCLUDE\w*=\w*', insertion + ',', config, flags=re.IGNORECASE | re.MULTILINE)
+        # Global section, no INCLUDE pair
         else:
-            config = insertion + config
+            config = config.replace('[global]', '[global]\n' + insertion)
+
         with open(os.path.expanduser(path), 'w') as file:
             file.write(config)
 
     @staticmethod
     def set_keyname(path):
+        username = os.getenv("SUDO_USER") or getpass.getuser()
         config = RegisterPluginDecorator.get_config(path)
-        config = config.replace('KEYNAME = AWSKey', 'KEYNAME = {}Key'.format(getpass.getuser()))
+        config = config.replace('KEYNAME = AWSKey', 'KEYNAME = {}Key'.format(username))
         with open(os.path.expanduser(path), 'w') as file:
             file.write(config)
 
@@ -52,7 +68,7 @@ setup(
     license='BSD',
     include_package_data=True,
     packages=[],
-    scripts=[plugin_name],
+    scripts=[],
     data_files=[(os.path.expanduser('~/.starcluster/plugins'), [plugin_name]),
                 (os.path.expanduser('~/.starcluster'), [config_name])],
     install_requires=['StarCluster >= 0.95.6'],
